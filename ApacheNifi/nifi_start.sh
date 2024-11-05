2d3078e16da335e259b133dd4758126cefd107cb
Peter Molnar <pmolnar@gsu.edu>
2024-11-05 19:40:48 +0000

    update

--- ApacheNifi/nifi_start.sh
+++ ApacheNifi/nifi_start.sh
@@ -3,6 +3,7 @@ podman stop ${USER}_nifi
 podman rm ${USER}_nifi
 podman run --name ${USER}_nifi \
 -v ./certs/localhost:/opt/certs \
+-p 28080:8080 \
 -p 28443:8443 \
 -e AUTH=tls \
 -e KEYSTORE_PATH=/opt/certs/keystore.jks \
@@ -12,4 +13,8 @@ podman run --name ${USER}_nifi \
 -e TRUSTSTORE_PASSWORD="Rw0sqHR15KAwn4Jw4nVBrHw+FAFWhyu1lcrgrV90J/Q=" \
 -e TRUSTSTORE_TYPE=JKS \
 -e INITIAL_ADMIN_IDENTITY="CN=$HOSTNAME, OU=InstituteForInsight, O=GSU, C=US" \
--d apache/nifi:latest
+-e NIFI_WEB_PROXY_CONTEXT_PATH=/nifi \
+-e NIFI_WEB_PROXY_HOST=compute-05.insight.gsu.edu \
+-d apache/nifi:1.5.0
+podman ps
+podman logs -f ${USER}_nifi
